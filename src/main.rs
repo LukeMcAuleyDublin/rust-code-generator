b9dfcba051633dabb59395767943e09582f01ae6
Luke McAuley <55586654+LukeMcAuleyDublin@users.noreply.github.com>
2022-12-11 22:04:24 +0000

    Added code

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::io::Read;
+use std::process::Command;
+
+fn main() {
+    let _alphabet = vec![
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1',
+        '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+        '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '\'', '"', ',', '.', '<', '>', '/', '?',
+        '`', '~', ' ', '\t',
+    ];
+    let mut file = std::fs::File::open("file.py").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let result = Command::new("python")
+        .args(&["file.py"])
+        .status()
+        .expect("Failed to run python script :(");
+    println!("status: {}", result);
+}
